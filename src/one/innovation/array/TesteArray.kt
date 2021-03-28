109df726f8b5578120a03f8970e53178dd0ca894
Jaco Miranda <jaco-miranda@hotmail.com>
2021-03-28 16:44:31 -0400

    Iniciando aprendizado de Array em Kotlin

---
+++ src/one/innovation/array/TesteArray.kt
@@ -0,0 +1,14 @@
+package one.innovation.array
+
+fun main() {
+    val values = IntArray(5)
+
+    values[0]= 7
+    values[1]= 3
+    values[2]= 6
+    values[3]= 2
+    values[4]= 4
+
+    for (valor in values)
+    println(valor)
+}
